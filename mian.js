f06ffd67ba2102392ba3f5cc5ad3e045d9ed9b43
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-14 11:46:02 +0100

    wgwrg

---
+++ mian.js
@@ -0,0 +1,15 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+const firstNumber = Number(prompt('inserisci il primo valore'))
+const secondNumber = Number(prompt('inserisci il secondo valore'))
+
+let bigNumber;
+
+if (firstNumber > secondNumber) {
+    bigNumber = firstNumber
+} else if (secondNumber > firstNumber) {
+    bigNumber = secondNumber
+}
+
+console.log(bigNumber)
