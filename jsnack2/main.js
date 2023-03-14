c9deca248a731d3c7b0b30752aa678032d54e263
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-14 12:07:53 +0100

    commit

---
+++ jsnack2/main.js
@@ -0,0 +1,15 @@
+/* L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga. */
+
+const firstWord = prompt('scirivi una parola')
+const secondWord = prompt('scirivi un\'altra parola')
+
+if (firstWord.length < secondWord.length) {
+    console.log(firstWord)
+    console.log(secondWord)
+} else if ( firstWord.length > secondWord.lengt ) {
+    console.log(secondWord)
+    console.log(firstWord)
+} else {
+    console.log('le parole hanno la stessa lunghezza')
+}
