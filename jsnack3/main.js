7ec4ba250f4359896c3382a7addc13d991e8472b
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-15 11:47:49 +0100

    add while

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,9 +1,18 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
-let number = 0;
+/* let number = 0;
 for (let i = 0; i < 10; i++) {
  number += Number(prompt('scirvi un numero'))
+}
+
+console.log(number) */
+
+let number = 0
+let i = 0
+while ( i < 10) {
+ number += Number(prompt('scirvi un numero'))
+ i++
 }
 
 console.log(number)
